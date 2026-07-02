a1af4e22118cf9de2b4555de0989bf461a270a69
VicariusDei <davidecool@gmail.com>
2026-07-02 15:35:35 +0200

    Add Capacitor config

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'it.friabili.mydndparty',
+  appName: 'MyDndParty',
+  webDir: '../dist'
+};
+
+export default config;
