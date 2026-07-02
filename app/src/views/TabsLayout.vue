aa5c6532bdec6f48aa9d9e5a8bde1044304c4121
VicariusDei <davidecool@gmail.com>
2026-07-03 00:31:33 +0200

    feat: add campaigns to bottom navigation

--- app/src/views/TabsLayout.vue
+++ app/src/views/TabsLayout.vue
@@ -7,6 +7,10 @@
           <ion-icon :icon="homeOutline" />
           <ion-label>Dashboard</ion-label>
         </ion-tab-button>
+        <ion-tab-button tab="campaigns" :href="tabHref('/tabs/campaigns')">
+          <ion-icon :icon="mapOutline" />
+          <ion-label>Campagne</ion-label>
+        </ion-tab-button>
         <ion-tab-button tab="party" :href="tabHref('/tabs/party')">
           <ion-icon :icon="peopleOutline" />
           <ion-label>Party</ion-label>
@@ -18,10 +22,6 @@
         <ion-tab-button tab="combat" :href="tabHref('/tabs/combat')">
           <ion-icon :icon="flashOutline" />
           <ion-label>Combattimento</ion-label>
-        </ion-tab-button>
-        <ion-tab-button tab="more" :href="tabHref('/tabs/more')">
-          <ion-icon :icon="ellipsisHorizontalCircleOutline" />
-          <ion-label>Altro</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -40,9 +40,9 @@ import {
 } from '@ionic/vue';
 import {
   cubeOutline,
-  ellipsisHorizontalCircleOutline,
   flashOutline,
   homeOutline,
+  mapOutline,
   peopleOutline
 } from 'ionicons/icons';
 
