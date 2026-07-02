90472cb4f267afb0f59b818ff5232dc202808f10
VicariusDei <davidecool@gmail.com>
2026-07-02 23:04:46 +0200

    fix: make bottom tabs compatible with app base path

--- app/src/views/TabsLayout.vue
+++ app/src/views/TabsLayout.vue
@@ -3,23 +3,23 @@
     <ion-tabs>
       <ion-router-outlet />
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="dashboard" href="/tabs/dashboard">
+        <ion-tab-button tab="dashboard" :href="tabHref('/tabs/dashboard')">
           <ion-icon :icon="homeOutline" />
           <ion-label>Dashboard</ion-label>
         </ion-tab-button>
-        <ion-tab-button tab="party" href="/tabs/party">
+        <ion-tab-button tab="party" :href="tabHref('/tabs/party')">
           <ion-icon :icon="peopleOutline" />
           <ion-label>Party</ion-label>
         </ion-tab-button>
-        <ion-tab-button tab="inventory" href="/tabs/inventory">
+        <ion-tab-button tab="inventory" :href="tabHref('/tabs/inventory')">
           <ion-icon :icon="cubeOutline" />
           <ion-label>Inventario</ion-label>
         </ion-tab-button>
-        <ion-tab-button tab="combat" href="/tabs/combat">
+        <ion-tab-button tab="combat" :href="tabHref('/tabs/combat')">
           <ion-icon :icon="flashOutline" />
-          <ion-label>Sessione</ion-label>
+          <ion-label>Combattimento</ion-label>
         </ion-tab-button>
-        <ion-tab-button tab="more" href="/tabs/more">
+        <ion-tab-button tab="more" :href="tabHref('/tabs/more')">
           <ion-icon :icon="ellipsisHorizontalCircleOutline" />
           <ion-label>Altro</ion-label>
         </ion-tab-button>
@@ -45,4 +45,10 @@ import {
   homeOutline,
   peopleOutline
 } from 'ionicons/icons';
+
+const appBase = import.meta.env.BASE_URL.replace(/\/$/, '');
+
+function tabHref(path: string) {
+  return `${appBase}${path}`;
+}
 </script>
