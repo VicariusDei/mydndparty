1e1d4911cb094a6a1e66621a488b213b02e9e888
VicariusDei <davidecool@gmail.com>
2026-07-02 15:33:21 +0200

    Add API config example

---
+++ api/config/config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'app_env' => 'local',
+    'app_debug' => true,
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'mydndparty',
+        'user' => 'db_user',
+        'pass' => 'db_password',
+        'charset' => 'utf8mb4',
+    ],
+];
